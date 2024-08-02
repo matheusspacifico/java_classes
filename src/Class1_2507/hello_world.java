7196ac881a4d3b8a8d05221965c44025da4d6f4b
Matheus <128740216+matheusspacifico@users.noreply.github.com>
2024-08-01 22:01:18 -0300

    just testing

--- src/Class1_2507/hello_world.java
+++ src/Class1_2507/hello_world.java
@@ -1,7 +1,7 @@
 package Class1_2507;
 
 public class hello_world {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
